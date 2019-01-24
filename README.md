4b53325a19f89b8e1b44bf54fe5a3be97bdf0113
Tom Helmuth <thelmuth@hamilton.edu>
2019-01-24 12:52:45 -0500

    Added actual code from first class.

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains all code from Tom Helmuth's CS110 class.
---
+++ first.py
@@ -0,0 +1,5 @@
+
+start = int(input("Enter the start year: "))
+end = int(input("Enter the end year: "))
+answer = (end - start) * 365
+print("The number of days is", answer)
