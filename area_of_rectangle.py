b3b1465142998396f5bc869c8c8ca96161496436
Tom Helmuth <thelmuth@hamilton.edu>
2019-01-25 09:51:13 -0500

    Area of rectangle program

---
+++ area_of_rectangle.py
@@ -0,0 +1,14 @@
+
+def main():
+    length = float(input("Enter the length of the rectangle: "))
+    width = float(input("Enter the width of the rectangle: "))
+    area = length * width
+    print("The area of the rectangle is", area)
+
+
+
+
+# this invokes the main function.  It is always included in our
+# python programs
+if __name__ == "__main__":
+    main()
