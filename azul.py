3bef2f1bbb7dc6be742cf7f5f0b0a0c3e93575c3
Tom Helmuth <thelmuth@hamilton.edu>
2019-03-15 10:48:40 -0400

    Worked on Azul: Azul and Factory classes

--- azul.py
+++ azul.py
@@ -7,6 +7,38 @@ import random
 
 TILE_COLORS = ["blue", "yellow", "red", "black", "white"]
 
+
+class Azul:
+    """Represent the entire game of Azul"""
+
+    def __init__(self):
+
+        # For now, assume a 2-player game
+        NUMBER_OF_PLAYERS = 2
+        NUMBER_OF_FACTORIES = 5
+
+        self._bag = Bag()
+
+        # Create the factories
+        self._factories = []
+        for _ in range(NUMBER_OF_FACTORIES):
+            self._factories.append(Factory())
+
+
+    def __str__(self):
+        """ We want this to look like:
+        ----- Azul -----
+        0. Factory with tiles: [<white>, <red>, <black>]
+        1. Factory with tiles: []
+        2. Factory ...
+        """
+
+        result = "----- Azul -----\n"
+        for index in range(len(self._factories)):
+            result += str(index) + ". " + str(self._factories[index]) + "\n"
+
+        return result
+
 class Tile:
     """Represents tiles of 5 colors"""
 
@@ -59,22 +91,61 @@ class Bag:
         return len(self._bag) == 0
 
 
+class Factory:
+    """Represents the factories in Azul"""
+
+    def __init__(self):
+        self._my_tiles = []
+
+    def add_tile(self, tile):
+        """Adds a tile to the factory"""
+        self._my_tiles.append(tile)
+
+    def __str__(self):
+        return "Factory with tiles: " + str(self._my_tiles)
+
+    def take_all_one_color(self, color):
+        """Remove and return all tiles of specified color from factory."""
+        return_list = []
+        retain_list = []
+
+        for tile in self._my_tiles:
+            if tile.get_color() == color:
+                return_list.append(tile)
+            else:
+                retain_list.append(tile)
+
+        # print(return_list)
+        # print(retain_list)
+
+        self._my_tiles = retain_list
+
+        return return_list
 
 
 def main():
 
-    tile_bag = Bag()
+    game = Azul()
 
-    while not tile_bag.empty():
-        print(len(tile_bag))
-        t = tile_bag.random_tile()
-
-        print(t)
+    print(game)
 
 
-
-
-
+    # tile_bag = Bag()
+    #
+    # factory1 = Factory()
+    #
+    # for _ in range(4):
+    #     tile = tile_bag.random_tile()
+    #     factory1.add_tile(tile)
+    #
+    # print(factory1)
+    #
+    # color = input("Enter a color you want take from this factory: ")
+    #
+    # users_tiles = factory1.take_all_one_color(color)
+    # print()
+    # print("User's tiles: ", users_tiles)
+    # print(factory1)
 
 
 
